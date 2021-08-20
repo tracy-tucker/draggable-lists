c3ee7cefc5328a4a34b6cc35ee301f9793086d79
ttucker <tracytuckeremail@gmail.com>
2021-08-20 12:41:46 -0500

    Basic structure of function flow and imports for App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import dataset from './dataset'
+import Column from './Column'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 
-function App() {
+const Container = styled.div`
+  display: flex;
+`
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
