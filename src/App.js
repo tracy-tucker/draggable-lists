3cc366761e833c42da5ab11a738f0d38d2e2bd3a
ttucker <tracytuckeremail@gmail.com>
2021-08-20 13:16:23 -0500

    additional comments

--- src/App.js
+++ src/App.js
@@ -41,3 +41,12 @@ export default App;
 // Draggable = a component that can be dragged around.
 // The DragDropContext expects a mandatory event, onDragEnd, which gets triggered whenever a Draggable has finished dragging from one point to another.
 
+// droppableId = just a unique identifier
+
+// Beautiful DND API requires a function that returns a React Component as children.
+// This function gets 2 arguments, provided and snapshot.
+
+// provided.innerRef = you must bind the provided.innerRef to the highest possible DOM node within the React element. This helps avoid using ReactDOM.
+// provided.droppableProps = This is an Object that contains properties that need to be applied to a Droppable element. It needs to be applied to the same element that you apply provided.innerRef to.
+
+
