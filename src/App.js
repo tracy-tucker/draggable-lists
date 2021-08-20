d9bd8857800c71ac382cfb1a58c818764c3149de
ttucker <tracytuckeremail@gmail.com>
2021-08-20 12:56:03 -0500

    Draggable and DDropple components addition to App.js

--- src/App.js
+++ src/App.js
@@ -15,8 +15,21 @@ const onDragEnd = result => {}
 
 const App = () => {
   return (
-    <div>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId='all-columns' direction='horizontal' type='column'>
+        {(provided) => (
+          <Container {...provided.droppableProps} ref={provided.innerRef}>
+            {data.columnOrder.at.map((id, index) => {
+              const column = data.columns[id]
+              const tasks = column.taskIds.map(taskId => data.tasks[taskId])
+
+              return <Column key={column.id} column={column} tasks={tasks} index={index} />
+            })}
+            {provided.placeholder}
+          </Container>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
 
