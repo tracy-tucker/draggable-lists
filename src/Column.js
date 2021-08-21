48b6a2ada866f7f0c7dde1555b66cf36a3dc4fa6
ttucker <tracytuckeremail@gmail.com>
2021-08-21 12:35:39 -0500

    code clean-up. Typos

--- src/Column.js
+++ src/Column.js
@@ -4,22 +4,24 @@ import Task from './Task'
 import { Droppable, Draggable } from 'react-beautiful-dnd'
 
 const Container = styled.div`
-    margin: 8px;
+    margin: 1rem;
     border: 1px solid lightgrey;
-    border-radius: 2px;
-    width: 220px;
+    border-radius: 5px;
+    width: 250px;
     display: flex;
     flex-direction: column;
     background-color: white;
-    `
+`
 const Title = styled.h3`
-    padding: 8px;
-    `
+    padding: 0 1rem;
+    margin: 1rem 0;
+`
 
 const TaskList = styled.div`
-    padding: 8px;
-    background-color: ${props => (props.isDraggingOver ? 'skyblue' : 'inherit')}
-    `
+    padding: 1rem;
+    background-color: ${props => (props.isDraggingOver ? '#d5f3ff' : 'inherit')}
+    min-height: 100px;
+`
 
 function Column(props) {
     return (
@@ -27,13 +29,13 @@ function Column(props) {
             {(provided) => (
                 <Container
                     ref={provided.innerRef}
-                    {...provided.dragableProps}>
+                    {...provided.draggableProps}>
                         <Title {...provided.dragHandleProps}>{props.column.title}</Title>
                     <Droppable droppableId={props.column.id} type="task">
                         {(provided, snapshot) => (
                             <TaskList
                                 ref={provided.innerRef}
-                                {...provided.drobbaleProps}
+                                {...provided.droppaleProps}
                                 isDraggingOver={snapshot.isDraggingOver}
                             >
                                 {props.tasks.map((task, index) => <Task key={task.id} task={task} index={index} />)}
@@ -42,7 +44,7 @@ function Column(props) {
                         )}
                     </Droppable>
                 </Container>
-                
+
             )}
         </Draggable>
     )
